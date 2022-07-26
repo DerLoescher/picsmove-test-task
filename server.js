8aa35ec7ab4440b986c2c8076cdd9e29c46f7e5b
Aleksey <alexdyachkin2000@bk.ru>
2022-07-26 17:39:48 +0300

    heroku deploy 1.2

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get("/", function (req, res) {
   res.render(path.join(__dirname + "/dist/index.html"));
 });
 
-var server = app.listen(process.env.PORT || 8080, function () {
-  var port = server.address().port;
+let server = app.listen(process.env.PORT || 8080, function () {
+  let port = server.address().port;
   console.log("App now running on port", port);
 });
